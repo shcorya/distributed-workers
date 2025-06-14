e5e0d7d54875cbd1c3dc81a72580f8c43b296ac2
Stephen Corya <stephen@corya.net>
2025-06-14 14:54:39 -0400

    feat: restart process on failure

--- worker.mjs
+++ worker.mjs
@@ -28,27 +28,22 @@ work();
 async function work() {
     // run in a loop
     while (true) {
-        try {
 
-            // get a job from the beanstalk queue
-            const { id, payload } = await beanstalk.reserve();
+        // get a job from the beanstalk queue
+        const { id, payload } = await beanstalk.reserve();
 
-            // hash the job's payload
-            const hash = crypto.createHash('md5').update(JSON.stringify(payload)).digest('hex');
+        // hash the job's payload
+        const hash = crypto.createHash('md5').update(JSON.stringify(payload)).digest('hex');
 
-            // delay up to 5 seconds to simulate a long-running job
-            await sleep(Math.floor(Math.random() * 5000));
+        // delay up to 5 seconds to simulate a long-running job
+        await sleep(Math.floor(Math.random() * 5000));
 
-            // add the completed job to mongodb by its beanstalk id
-            await completed.insertOne({_id: id, hash, payload});
+        // add the completed job to mongodb by its beanstalk id
+        await completed.insertOne({_id: id, hash, payload});
 
-            // delete the job (by its id) from the queue when it is done and added to mongodb
-            await beanstalk.delete(id);
+        // delete the job (by its id) from the queue when it is done and added to mongodb
+        await beanstalk.delete(id);
 
-        } catch (error) {
-            // log the error for debugging
-            console.error(error);
-        }
     }
 }
 
