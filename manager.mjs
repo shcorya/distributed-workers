1e2f0e8b4b4374edf59a898ab5697b20df445cea
Stephen Corya <stephen@corya.net>
2025-06-16 18:15:08 -0400

    fix: change localhost to local IP address

--- manager.mjs
+++ manager.mjs
@@ -18,14 +18,14 @@ import { MongoClient } from "mongodb";
 // initalize the beanstalk client
 const beanstalk = new Jackd({
     autoconnect: false,
-    host: process.env.BEANSTALK_HOST || 'localhost',
+    host: process.env.BEANSTALK_HOST || '127.0.0.1',
     port: Number.parseInt(process.env.BEANSTALK_PORT) || 11300,
     maxReconnectAttempts: Number.parseInt(process.env.BEANSTALK_MAX_RECONNECT_ATTEMPTS) || 3
 });
 await beanstalk.connect();
 
 // initialize the mongodb client, database, and collection
-const mongo = new MongoClient(process.env.MONGO_URL || 'mongodb://localhost:27017');
+const mongo = new MongoClient(process.env.MONGO_URL || 'mongodb://127.0.0.1:27017');
 await mongo.connect();
 const db = mongo.db('jobs');
 const completed = db.collection('completed');
