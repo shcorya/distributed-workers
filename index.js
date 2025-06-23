80688fc197bd16b28927df847e6f11b60530e4cc
Stephen Corya <stephen@corya.net>
2025-06-23 13:26:46 -0400

    fix: change require to import

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
 
 // basic CLI that dictates which module to use (API manager or distributed worker)
-if (process.argv.includes("manager")) require("./manager.mjs");
-else if (process.argv.includes("worker")) require("./worker.mjs");
+if (process.argv.includes("manager")) import("./manager.mjs");
+else if (process.argv.includes("worker")) import("./worker.mjs");
